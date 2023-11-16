c9a872dfe1f05bd80bcf5250e52ad162a9fce631
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-11-16 13:14:30 +0100

    react-leaflet test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
 
-const App = () => {
+function App() {
+  const highlightedArea = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [-0.09, 51.505],
+          [-0.08, 51.505],
+          [-0.08, 51.506],
+          [-0.09, 51.506],
+          [-0.09, 51.505],
+        ],
+      ],
+    },
+  };
+
   return (
-    <div className="">Mappy</div>
-  )
+    <MapContainer center={[51.505, -0.09]} zoom={20} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          Les Corts
+        </Popup>
+      </Marker>
+      <GeoJSON
+        data={highlightedArea}
+        style={() => ({
+          fillColor: 'blue',
+          color: 'red',
+          weight: 2,
+          opacity: 0.5,
+        })}
+      />
+    </MapContainer>
+  );
 }
 
-export default App
+export default App;
