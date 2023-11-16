fd2b85a04388c070f954ebd6a5267371ed14428c
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-11-16 12:14:14 +0100

    project setup ready

---
+++ mappy/.prettierrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: ['prettier-plugin-tailwindcss'],
+    singleQuote: true,
+    semi: false,
+    tabWidth: 4,
+    // Add more formatting options as needed
+}
---
+++ mappy/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>Mappy</div>
+  )
+}
+
+export default App
